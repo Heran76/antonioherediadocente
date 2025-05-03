4947c914a85aed67d0d6b6ea8a5e4043be4c0cf8
Heran76 <heran76@gmail.com>
2025-05-03 12:03:30 +0200

    front

--- elerning_plarform/Herandoc/src/components/header.jsx
+++ elerning_plarform/Herandoc/src/components/header.jsx
@@ -1,16 +1,24 @@
 import '../styles/header.css';
 import { Link } from 'react-router-dom';
+import userIcon from '../assets/img/agregar-usuario.png'; // Asegúrate de que la ruta sea correcta
 
 export default function Header() {
   return (
     <header className="main-header">
-    
       <nav className="main-nav">
         <Link to="/">Inicio</Link>
         <Link to="/academia">Academia</Link>
         <Link to="/sobre-mi">Sobre mí</Link>
         <Link to="/contacto">Contacto</Link>
       </nav>
+      
+      <div className="auth-links">
+        <Link to="/acceder" className="auth-link">
+          <img src={userIcon} alt="Icono de usuario" className="user-icon" />
+          Acceder
+        </Link>
+        <Link to="/registro" className="auth-link">Registrarse</Link>
+      </div>
     </header>
   );
 }
