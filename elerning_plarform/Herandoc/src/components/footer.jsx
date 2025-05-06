0aab17e3066c8e8d88c681ce7bb4bb58d6b07425
Heran76 <heran76@gmail.com>
2025-05-06 15:08:04 +0200

    Martes

--- elerning_plarform/Herandoc/src/components/footer.jsx
+++ elerning_plarform/Herandoc/src/components/footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="main-footer">
       <div className="copyright">
-        © Todos los derechos reservados Heran76 2025
+        © Todos los derechos reservados  Heran76 2025
       </div>
       <nav className="footer-nav">
         <a href="/politica-privacidad">Política de privacidad</a>
