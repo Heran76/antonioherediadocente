5e2cb53ae71595ce173fb560a489de22b583753a
Heran76 <heran76@gmail.com>
2025-05-10 12:16:33 +0200

    nueva configuracion

--- elerning_plarform/Herandoc/src/firebase/firebase.js
+++ elerning_plarform/Herandoc/src/firebase/firebase.js
@@ -1,29 +1,23 @@
-// Import the functions you need from the SDKs you need
+// Importa los servicios de Firebase que necesitas
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Configuración de TU NUEVO PROYECTO (heran76devacademy)
 const firebaseConfig = {
-  apiKey: "AIzaSyAc-y774IaHxO_uveTQDbv74ETWX0mDlLs",
-  authDomain: "herandoc-elearning.firebaseapp.com",
-  projectId: "herandoc-elearning",
-  storageBucket: "herandoc-elearning.firebasestorage.app",
-  messagingSenderId: "501669850120",
-  appId: "1:501669850120:web:89b4d9df1d16af8085f5bd",
-  measurementId: "G-TDJ81W7TH4"
+  apiKey: "AIzaSyBBIZG0f59ZAE6t2_f6Zcl-Q82IVUz_nkk",
+  authDomain: "heran76devacademy.firebaseapp.com",
+  projectId: "heran76devacademy",
+  storageBucket: "heran76devacademy.firebasestorage.app",
+  messagingSenderId: "119841811283",
+  appId: "1:119841811283:web:d166567a3d1127ad734fa6"
 };
 
-// Initialize Firebase
+// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-// Exporta los servicios que necesitarás
+
+// Exporta los servicios que usarás en tu app
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
