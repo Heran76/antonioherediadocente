08b9d84769377a1e291874bf3a36c5552b0db35f
Heran76 <heran76@gmail.com>
2025-05-20 15:00:34 +0200

    martes

--- elerning_plarform/Herandoc/src/components/login.jsx
+++ elerning_plarform/Herandoc/src/components/login.jsx
@@ -22,7 +22,7 @@ export default function Login() {
         type="email" 
         value={email} 
         onChange={(e) => setEmail(e.target.value)} 
-        placeholder="Correos electrónicos" 
+        placeholder="Correo electrónico" 
       />
       <input 
         type="password" 
