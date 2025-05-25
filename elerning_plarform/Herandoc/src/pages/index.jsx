d2352f6802043887d00c109d21e1c5bab1704a86
Heran76 <heran76@gmail.com>
2025-05-25 10:52:07 +0200

    Domingo

--- elerning_plarform/Herandoc/src/pages/index.jsx
+++ elerning_plarform/Herandoc/src/pages/index.jsx
@@ -9,9 +9,7 @@ export default function Home() {
       {/* Sección de prueba con Tailwind */}
       <div className="max-w-md mx-auto mt-8 p-6 bg-white rounded-lg shadow-lg">
         <h2 className="text-2xl font-semibold text-blue-600">¡Tailwind está funcionando!</h2>
-        <p className="mt-4 text-gray-700">
-          Si ves este recuadro con bordes redondeados y sombra, Tailwind CSS está configurado correctamente.
-        </p>
+       
         <button className="mt-4 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">
           Botón de prueba
         </button>
